17af6db85c6b2df1bb86a542980d47444bc759f4
Sakib-01 <syedsakibulhaque2@gmail.com>
2024-11-01 20:16:09 +0600

    show book card

---
+++ src/components/Book/Book.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const Book = ({ book }) => {
+  const {
+    bookId,
+    bookName,
+    author,
+    image,
+    review,
+    totalPages,
+    rating,
+    category,
+    tags,
+    publisher,
+    yearOfPublishing,
+  } = book;
+  return (
+    <div>
+      <div className="card bg-base-100 w-96 shadow-xl border-2 border-green-800">
+        <figure className="bg-gray-100 p-10 rounded-3xl">
+          <img className="w-20  h-24 " src={image} alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <div className="flex gap-5 ">
+            {tags.map((tag) => (
+              <div className=" badge badge-outline text-green-600  p-4">
+                {tag}
+              </div>
+            ))}
+          </div>
+          <h2 className="card-title text-2xl text-gray-100 text-left">
+            {bookName}
+          </h2>
+          <p className="text-xl text-gray-400 text-left">By:{author}</p>
+          <p className="text-base text-gray-400 text-left">{category}</p>
+          <hr className="border-dashed" />
+          <div className="card-actions justify-start">
+            <div className="badge badge-outline p-4"> Rating: {rating}</div>
+            <div className="badge badge-outline p-4">{totalPages} pages</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Book;
--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Book from "../Book/Book";
 
 const Books = () => {
   const [books, setBooks] = useState([]);
@@ -9,7 +10,14 @@ const Books = () => {
   }, []);
   return (
     <div>
-      <h2 className="text-5xl text-white">Books {books.length}</h2>
+      <h2 className="text-5xl text-white my-10">
+        {books.length} Book Available
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {books.map((book) => (
+          <Book key={book.bookId} book={book}></Book>
+        ))}
+      </div>
     </div>
   );
 };
